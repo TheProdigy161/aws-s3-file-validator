01d88091a8876049d14bf0b5efae125436da07db
TheProdigy161 <rossbratton20@gmail.com>
2024-11-10 20:33:54 +0000

    Updated to allow for S3 event to be accepted

--- src/aws-s3-file-validator/Function.cs
+++ src/aws-s3-file-validator/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,8 +8,12 @@ namespace aws_s3_file_validator;
 
 public class Function
 {
-    public string Run(string input, ILambdaContext context)
+    public void Run(S3Event s3Event)
     {
-        return input.ToUpper();
+        Console.WriteLine("Processing S3 Event");
+
+        Console.WriteLine("Bucket Name: " + s3Event.Records[0].S3.Bucket.Name);
+
+        Console.WriteLine("Object Key: " + s3Event.Records[0].S3.Object.Key);
     }
 }
